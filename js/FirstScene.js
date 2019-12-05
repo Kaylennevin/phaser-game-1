f1eec7eadebf04da1d27dd62de92709f7ac41d25
Kaylennevin <55703629+Kaylennevin@users.noreply.github.com>
2019-12-05 16:19:34 +0000

    moving platform
    block variable
    moving platform "block"
    tween

--- js/FirstScene.js
+++ js/FirstScene.js
@@ -2,11 +2,13 @@ class FirstScene extends Phaser.Scene {
     player;
     cursors;
     platforms;
+    block;
     gems;
     bomb;
     scoreText;
     score = 0;
     gameOver = false;
+    
 
     constructor(config) {
         super(config);
@@ -15,6 +17,7 @@ class FirstScene extends Phaser.Scene {
         this.load.image("hills", "assets/png/Background.png");
         this.load.image("ground-1600", "assets/platform-1600.png");
         this.load.image("platform-100", "assets/platform-100.png");
+        this.load.image("block", "assets/moving-platform.png");
         this.load.image("gem", "assets/png/gem3.png");
         this.load.image("bomb", "assets/bomb.png");
         this.load.spritesheet("dude", "assets/dude.png", {
@@ -38,25 +41,65 @@ class FirstScene extends Phaser.Scene {
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(800, 585, "ground-1600");
         this.platforms.create(250, 500, "platform-100");
-        this.platforms.create(150, 400, "platform-100");
         this.platforms.create(350, 275, "platform-100");
         this.platforms.create(350, 150, "platform-100");
         this.platforms.create(650, 110, "platform-100");
         this.platforms.create(550, 110, "platform-100");
 
+
+        // let bwidth = 30;
+        // let bheight = 30;
+        // let origin = {x:16, y:400};
+        // let dest = {x:16, y:50};
+        // let line = new Phaser.Curves.Line(origin, dest);
+        // this.baddie = scene.add.follower(line, origin.x, origin.y, "baddie")
+        // this.baddie.startFollow({
+        //     duration: 3000,
+        //     repeat: -1,
+        //     yoyo: true
+
+        // });
+
+
+
+var block = this.physics.add.image(100, 500, 'block')
+.setImmovable(true)
+.setVelocity(0, 0);
+
+block.body.setAllowGravity(false);
+
+this.tweens.timeline({
+    targets: block.body.velocity,
+    loop: -1,
+    tweens: [
+      { x:    0, y: -200, duration: 2000, ease: 'Stepped' },
+      { x:    0, y: 200, duration: 2000, ease: 'Stepped' },
+     
+      
+     
+    ]
+  });
+
+        
+
+  
+      
+
         this.bombs = this.physics.add.group();
         this.physics.add.collider(this.bombs, this.bombs);
         this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(this.bombs, block);
+        this.physics.add.collider(this.player, block);
         this.physics.add.overlap(this.bombs, this.player, this.endGame, null,this);
         
         this.gems = this.physics.add.group (
             {
                 key: "gem",
-                repeat: 6,
+                repeat: 144,
                 setXY:  {
                     X: 25,
                     Y: 0,
-                    stepX: 150,
+                    stepX: 15,
                 }
             }
         );
@@ -71,6 +114,8 @@ class FirstScene extends Phaser.Scene {
 
 
         this.physics.add.collider(this.gems, this.platforms);
+        this.physics.add.collider(this.gems, block);
+        
         this.physics.add.collider(this.player, this.platforms);
         this.physics.add.overlap(this.player, this.gems, this.collectGem, null, this);
         this.scoreText = this.add.text(20,20,"Score: 0",
@@ -136,7 +181,7 @@ class FirstScene extends Phaser.Scene {
         if (this.player.body.touching.down) {
             this.player.jumpCount = 0;
         }
-        this.makeBomb();
+        
     }
 }
    
@@ -150,16 +195,17 @@ class FirstScene extends Phaser.Scene {
                     child.enableBody(true, child.x, 0, true, true);
                 }
             )
+            this.makeBomb();
         }
     }
 
     makeBomb() {
         //let x = (this.player.x < 600) ? Phaser.Math.Between(1200, 3400) : Phaser.Math.Between(0, 1200);
-        let x = Phaser.Math.Between(0, 3400);
+        let x = Phaser.Math.Between(0, 1200);
         let bomb = this.bombs.create(x, 16, "bomb");
-        bomb.setBounce(0.5);
+        bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(0.5,0);
+        bomb.setVelocity(120,5);
 
     }
 
